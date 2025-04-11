08b6432e867c3dc0d97ac8986c85978973de3e7f
Bijay Sharma <58084675+bijaysharma8335@users.noreply.github.com>
2025-04-11 20:06:19 +0545

    backend setup

---
+++ Server/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
+const dbConnection = require("./utils/db");
+// const routes=require("./routes")
+
+dotenv.config();
+dbConnection();
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.use(
+    cors({
+        origin: ["http://localhost:3000", "http://localhost:3001"],
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        credentials: true,
+    })
+);
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(cookieParser());
+app.use(morgan("dev"));
+app.get("/", (req, res) => {
+    res.send("server is running");
+});
+
+
+// app.use("/api",routes)
+// app.use(routeNotFound)
+// app.use(errorHandler)
+
+app.listen(PORT, () => {
+    console.log(`Server is running at port ${PORT}`);
+});
