f3b77ed0837ede5c0f8239bded088fc5743493be
Bijay Sharma <58084675+bijaysharma8335@users.noreply.github.com>
2025-04-11 20:39:09 +0545

    create user  schema model

--- Server/middleware/errorMiddleware.js
+++ Server/middleware/errorMiddleware.js
@@ -1,3 +1,18 @@
-const routeNotFound=(req,res,next)=>{
-const error=new Error("Route not found")
-}
+const routeNotFound = (req, res, next) => {
+    const error = new Error(`Route not found: ${req.originalUrl}`);
+    res.status(404);
+    next(error);
+};
+
+const errorHandler = (err, req, res, next) => {
+    const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+    let message = err.message;
+    if (err.name === "CastError" && err.kind === "ObjectId") {
+        message = "Resource not found";
+    }
+    res.status(statusCode).join({
+        message: message,
+        stack: process.env.NODE_ENV === "production" ? null : err.stack,
+    });
+};
+export { routeNotFound, errorHandler };
